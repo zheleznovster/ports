5042b72e368795fb9356fe67dd69be1245f34e8b
Mykhailo <mykhailo@sosiv.io>
2022-07-31 22:44:23 +0200

    add signals

---
+++ signals/signal.go
@@ -0,0 +1,23 @@
+package signals
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// WaitForTerminationSignals handles os certain signals
+func WaitForTerminationSignals() os.Signal {
+	// creating a channel to listen for signals, like SIGINT
+	stop := make(chan os.Signal, 1)
+	// subscribing to interruption signals
+	signal.Notify(stop,
+		syscall.SIGHUP,  // terminal closed
+		syscall.SIGINT,  // CTRL+C pressed
+		syscall.SIGTERM, // terminate
+		syscall.SIGQUIT, // quit with core dump
+		syscall.SIGKILL) // process killed
+
+	// this blocks until the signal is received
+	return <-stop
+}
